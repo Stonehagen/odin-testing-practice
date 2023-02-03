4eb06e5f7a87c7460a95dd8a452fc5261aa982a1
Stonehagen <steinhagen@mailbox.org>
2023-02-03 23:13:22 +0100

    add reverseString method

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export const capitalize = (word) => word[0].toUpperCase() + word.slice(1);
+
+export const reverseString = (string) => string.split('').reverse().join('');
--- main.test.js
+++ main.test.js
@@ -1,7 +1,13 @@
-import { capitalize } from './main';
+import { capitalize, reverseString } from './main';
 
 describe('capatilize module', () => {
   test('capitalize the word test to be Test', () => {
     expect(capitalize('test')).toBe('Test');
+  });
+});
+
+describe('reverseString module', () => {
+  test('reverse the String Test to be tseT', () => {
+    expect(reverseString('Test')).toBe('tseT');
   });
 });
