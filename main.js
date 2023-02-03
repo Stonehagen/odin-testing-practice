3a543374f87cbd5ecae2514f2da02fe99feb72d7
Stonehagen <steinhagen@mailbox.org>
2023-02-03 23:22:45 +0100

    add calculator object and tests

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-// eslint-disable-next-line import/prefer-default-export
 export const capitalize = (word) => word[0].toUpperCase() + word.slice(1);
 
 export const reverseString = (string) => string.split('').reverse().join('');
+
+export const calculator = (() => {
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const divide = (a, b) => a / b;
+  const multiply = (a, b) => a * b;
+
+  return {
+    add,
+    subtract,
+    divide,
+    multiply,
+  };
+})();
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './main';
+import { capitalize, reverseString, calculator } from './main';
 
 describe('capatilize module', () => {
   test('capitalize the word test to be Test', () => {
@@ -9,5 +9,20 @@ describe('capatilize module', () => {
 describe('reverseString module', () => {
   test('reverse the String Test to be tseT', () => {
     expect(reverseString('Test')).toBe('tseT');
+  });
+});
+
+describe('calculator object', () => {
+  test('add the number 1 and 2 tobe 3', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+  });
+  test('subtract the number 9 and 8 tobe 1', () => {
+    expect(calculator.subtract(9, 8)).toBe(1);
+  });
+  test('divide 6 by 3 tobe 2', () => {
+    expect(calculator.divide(6, 3)).toBe(2);
+  });
+  test('multiplay 4 by 4 tobe 16', () => {
+    expect(calculator.multiply(4, 4)).toBe(16);
   });
 });
